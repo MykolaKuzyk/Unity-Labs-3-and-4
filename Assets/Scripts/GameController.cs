d319347e09a748af6f5308092b978ecb50a6bc57
Mykola Kuzyk <ukrboy99@gmail.com>
2021-09-19 19:42:11 +0200

    Unity project 3 and 4 learning

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class GameController : MonoBehaviour
+{
+    public GameObject OneBallPrefab;
+    public Text TextController;
+    public int BallNubmer;
+  
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+        InvokeRepeating("AddABall", 1.0F, 1.0F);
+        
+    }
+
+   public virtual int count(int ballnumber)
+    {
+        return BallNubmer;
+    }
+
+
+    void AddABall()
+    {
+        Instantiate(OneBallPrefab);
+        TextController.text = BallNubmer.ToString();
+        BallNubmer += 1;
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+  
+       
+        
+       
+    }
+}
---
+++ Assets/Scripts/TextController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextController : MonoBehaviour
+{
+    
+ 
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector3(3 , 3 , 3);
+       
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
