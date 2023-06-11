5948bc17f1ddf2d6767c9be88b6aeee78c6d20dd
Mykola Kuzyk <mikolaj.kuzyk@gmail.com>
2023-06-11 15:20:07 +0200

    Fixed View and Added new methods for destroing all balls Revert "Add .gitignore and .gitattributes."
    This reverts commit ca771894c3543c66699a38754f3569eedec9fd71.

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -8,16 +8,18 @@ public class GameController : MonoBehaviour
 {
     public GameObject OneBallPrefab;
     public Text TextController;
+    public Text TextScoreController;
     public int BallNubmer;
-  
-    
+    public int Score = 0;
+    public bool GameOver = true;
+    public Button PlayAgainButton;
 
     // Start is called before the first frame update
     void Start()
     {
        
         InvokeRepeating("AddABall", 1.0F, 1.0F);
-        
+     
     }
 
    public virtual int count(int ballnumber)
@@ -28,17 +30,59 @@ public class GameController : MonoBehaviour
 
     void AddABall()
     {
-        Instantiate(OneBallPrefab);
-        TextController.text = BallNubmer.ToString();
-        BallNubmer += 1;
-
+        if (!GameOver)
+        {
+            
+            Instantiate(OneBallPrefab);
+            BallNubmer += 1;
+            TextController.text = $"BALLS: {BallNubmer}";
+            
+        }
+        else
+        {
+            TextController.text = "Press Start Button";
+        }
     }
     // Update is called once per frame
     void Update()
     {
-  
-       
+    }
+    public void ClickedOnBall()
+    {
+        Score++;
+        BallNubmer--;
         
-       
+
+        if (BallNubmer <= 0)
+        {
+            GameOver = true;
+            PlayAgainButton.gameObject.SetActive(true);
+        }
+        else
+        {
+            if (BallNubmer > 20)
+            {
+                GameOver = true;
+                PlayAgainButton.gameObject.SetActive(true);
+                TextController.text = "Game Over too many balls";
+            }
+
+        }
+        TextScoreController.text = $"SOCRE: {Score}";
+    }
+    public void StartNewGame()
+    {
+        
+        Score = 0;
+        BallNubmer = 0;
+        GameOver = false;
+        PlayAgainButton.gameObject.SetActive(false);
+        TextController.text = "Press Start Button";
+        foreach (GameObject OneBallPrefab in GameObject.FindGameObjectsWithTag ("GameController"))
+        {
+            Destroy(OneBallPrefab);
+        }
+
+   
     }
 }
--- Assets/Scripts/OneBallBehavior.cs
+++ Assets/Scripts/OneBallBehavior.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class OneBallBehavior : MonoBehaviour
 {
-    public float XRotation = 0;
+    public float XRotation = -2;
     public float YRotation = 1;
     public float ZRotation = 0;
     public float DegreesPerSecond = 180;
@@ -13,16 +13,24 @@ public class OneBallBehavior : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        transform.position = new Vector3(3 - Random.value *6, 3 - Random.value *6, 3 - Random.value *6);
+        transform.position = new Vector3(761- Random.value *6, 359 - Random.value*6, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
         Vector3 axis = new Vector3(XRotation, YRotation, ZRotation);
-        transform.RotateAround(Vector3.zero, axis, DegreesPerSecond * Time.deltaTime);
-            //Debug.DrawRay(Vector3.zero, axis, Color.yellow);
+        transform.RotateAround(new Vector3(761, 357, 0), axis, DegreesPerSecond * Time.deltaTime);
 
-        
+        //Debug.DrawRay(new Vector3(761, 359, 0), axis, Color.yellow);
+    }
+    void OnMouseDown()
+    {
+        GameController controller = Camera.main.GetComponent<GameController>();
+        if (!controller.GameOver)
+        {
+            controller.ClickedOnBall();
+            Destroy(gameObject);
+        }
     }
 }
